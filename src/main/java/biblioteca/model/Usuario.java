6d2441767c0ed753a0bc51377c4b999e5ae41faf
garffield <gabrielfranklinbarcellos@gmail.com>
2024-10-29 17:13:16 -0300

    Atributos e Métodos

--- src/main/java/biblioteca/model/Usuario.java
+++ src/main/java/biblioteca/model/Usuario.java
@@ -9,7 +9,7 @@ public class Usuario implements Interface.interfaceCrud {
     public String cpf;
     public Boolean status;
 
-    public Usuario (String user, String endereco, Integer telefone, Integer email, String cpf, Boolean status) {
+    public Usuario (Integer idUsuario,String user, String endereco, Integer telefone, Integer email, String cpf, Boolean status) {
         this.idUsuario = idUsuario;
         this.user = user;
         this.endereco = endereco;
