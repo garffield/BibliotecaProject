2fe0768710f76c9289813817a3fa527375716aad
garffield <gabrielfranklinbarcellos@gmail.com>
2024-10-30 15:56:16 -0300

    Atributos e Métodos

--- src/main/java/biblioteca/model/Multa.java
+++ src/main/java/biblioteca/model/Multa.java
@@ -1,4 +1,7 @@
 package main.java.biblioteca.model;
 
 public class Multa {
+    //	Atributos: id, usuario, valor, dataGeracao, paga.
+    //	Métodos: calcularMulta(), registrarPagamento().
+    //
 }
